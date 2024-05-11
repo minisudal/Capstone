740d3000d330ff039746814803dc5c531b28112e
sudal <sukunz1228@naver.com>
2024-05-12 03:02:49 +0900

    Capstone

---
+++ app/src/main/java/com/example/test/TipFragment5.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TipFragment5 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_tip5);
+    }
+}
